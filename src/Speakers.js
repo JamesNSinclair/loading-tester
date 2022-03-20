910dc0ee63fbfd15da3b01b00157ebbc7586a03d
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2022-03-20 17:08:32 +0000

    created comp to stop speakers being rerendered

--- src/Speakers.js
+++ src/Speakers.js
@@ -4,16 +4,16 @@ import {Header} from './Header';
 import {Menu} from './Menu';
 import SpeakerData from "./SpeakerData";
 import SpeakerDetail from "./SpeakerDetail";
-import { useConfigContext } from "./ThemeContext";
+import SpeakerDays from "./SpeakerDays";
+
 
 const Speakers = ({}) => {
-    const [speakingSaturday, setSpeakingSaturday] = useState(true);
-  const [speakingSunday, setSpeakingSunday] = useState(true);
+
 
   const [speakerList, setSpeakerList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-    const context = useConfigContext();
+
 
     useEffect(() => {
         setIsLoading(true);
@@ -29,14 +29,7 @@ const Speakers = ({}) => {
             console.log('cleanup');
         };
     }, []);
-    const handleChangeSaturday = () => {
-        setSpeakingSaturday(!speakingSaturday);
-      };
-    
-      const handleChangeSunday = () => {
-        setSpeakingSunday(!speakingSunday);
-      };
-    
+
       const speakerListFiltered = isLoading
         ? []
         : speakerList
@@ -75,32 +68,7 @@ const Speakers = ({}) => {
           <Menu />
           <div className="container">
             <div className="btn-toolbar  margintopbottom5 checkbox-bigger">
-              {context.showSpeakerSpeakingDays === false ? null : (
-                <div className="hide">
-                  <div className="form-check-inline">
-                    <label className="form-check-label">
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        onChange={handleChangeSaturday}
-                        checked={speakingSaturday}
-                      />
-                      Saturday Speakers
-                    </label>
-                  </div>
-                  <div className="form-check-inline">
-                    <label className="form-check-label">
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        onChange={handleChangeSunday}
-                        checked={speakingSunday}
-                      />
-                      Sunday Speakers
-                    </label>
-                  </div>
-                </div>
-              )}
+              <SpeakerDays />
             </div>
             <div className="row">
               <div className="card-deck">
